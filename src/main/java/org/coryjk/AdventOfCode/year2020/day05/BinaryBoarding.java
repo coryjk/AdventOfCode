5c1ffeb6cdff178eb9ddeef155b6a62a9efc30c6
coryjk <36049396+coryjk@users.noreply.github.com>
2021-02-23 22:50:16 -0800

    Begin day 5

---
+++ src/main/java/org/coryjk/AdventOfCode/year2020/day05/BinaryBoarding.java
@@ -0,0 +1,34 @@
+package org.coryjk.AdventOfCode.year2020.day05;
+
+import org.coryjk.AdventOfCode.commons.SolutionLogger;
+
+public final class BinaryBoarding extends SolutionLogger {
+
+    /**
+     * Initializes state by loading all required resources.
+     */
+    @Override
+    public void feed() {
+
+    }
+
+    /**
+     * Returns solution to part 1 as a string.
+     *
+     * @return Answer.
+     */
+    @Override
+    public String solvePart1() {
+        return null;
+    }
+
+    /**
+     * Returns solution to part 2 as a string.
+     *
+     * @return Answer.
+     */
+    @Override
+    public String solvePart2() {
+        return null;
+    }
+}
