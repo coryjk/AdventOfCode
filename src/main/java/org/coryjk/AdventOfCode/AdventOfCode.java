8b026a103361fd9b72972657cacb53b0017b7709
coryjk <36049396+coryjk@users.noreply.github.com>
2021-02-26 22:22:50 -0800

    Generic call to solution drivers provided class list

--- src/main/java/org/coryjk/AdventOfCode/AdventOfCode.java
+++ src/main/java/org/coryjk/AdventOfCode/AdventOfCode.java
@@ -2,6 +2,7 @@ package org.coryjk.AdventOfCode;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.log4j.BasicConfigurator;
+import org.coryjk.AdventOfCode.commons.SolutionLogger;
 import org.coryjk.AdventOfCode.year2020.day01.ReportRepair;
 import org.coryjk.AdventOfCode.year2020.day02.PasswordPhilosophy;
 import org.coryjk.AdventOfCode.year2020.day03.TobogganTrajectory;
@@ -14,12 +15,25 @@ import org.coryjk.AdventOfCode.year2020.day07.HandyHaversacks;
 public final class AdventOfCode {
     public static void main(String[] args) {
         BasicConfigurator.configure();
-        new ReportRepair().solve();
-        new PasswordPhilosophy().solve();
-        new TobogganTrajectory().solve();
-        new PassportProcessing().solve();
-        new BinaryBoarding().solve();
-        new CustomCustoms().solve();
-        new HandyHaversacks().solve();
+        solve(
+                ReportRepair.class,
+                PasswordPhilosophy.class,
+                TobogganTrajectory.class,
+                PassportProcessing.class,
+                BinaryBoarding.class,
+                CustomCustoms.class,
+                HandyHaversacks.class
+        );
+    }
+
+    @SafeVarargs
+    private static void solve(final Class<? extends SolutionLogger> ... solutions) {
+        for (final Class<? extends SolutionLogger> solution : solutions) {
+            try {
+                solution.getConstructor().newInstance().solve();
+            } catch (Exception exception) {
+                log.error("Issue calling solution driver for " + solution.getName(), exception);
+            }
+        }
     }
 }
